c08bb304e7f9de46907a2deab4260a4f6463466b
liudmylai <liudmyla.iovenko@gmail.com>
2023-07-31 12:03:12 -0400

    Data preparation

--- dataframes.py
+++ dataframes.py
@@ -13,3 +13,20 @@ n = nutr_mini.iloc[0:3, col_vb12:]
 # get the calories for the third food in nutr_mini using an attribute-based approach that is faster than .loc or .iloc
 col_calories = nutr_mini.columns.get_loc('calories') # 1
 cal = nutr_mini.iat[2,1] # 185
+
+#Skill Challenge 2
+# data preparation
+# collect units
+units = df.astype(str).replace('[^a-zA-Z]','',regex=True).mode()
+# remove columns with empty values
+units = units.replace('', np.nan).dropna(axis=1)
+# rename labels (columns) to not lose data
+# create mapper
+mapper = {k : k + '_' + units[k].at[0] for k in units}
+nutritions = df.rename(columns=mapper)
+# remove units from values
+nutritions.replace('[a-zA-Z]','',regex=True,inplace=True)
+# cast to float
+nutritions = nutritions.astype(float)
+
+print(nutritions.head())
